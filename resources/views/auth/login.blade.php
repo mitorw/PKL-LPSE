b4b4db222e15abd0822e1af8da8d5a12da3b0102
Mitorw <mitorahmandhanawijaya@gmail.com>
2025-08-19 11:53:38 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -103,11 +103,6 @@
                 @enderror
             </div>
 
-            <!-- Remember Me -->
-            <div class="form-check mb-3">
-                <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
-                <label class="form-check-label" for="remember_me">Remember me</label>
-            </div>
 
             <!-- Actions -->
             <div class="d-flex justify-content-between align-items-center mb-3">
