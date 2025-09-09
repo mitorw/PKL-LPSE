c9aa66b9bd2ff4e41c81aa037095636efb19e972
masfiq28 <syafiqnasrullah2802@gmail.com>
2025-09-09 08:33:10 +0700

    Update Logo favicon browser

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Sistem Manajemen Surat</title>
+    <link rel="icon" type="image/png" href="{{ asset('../storage/assets/favicon.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css" />
